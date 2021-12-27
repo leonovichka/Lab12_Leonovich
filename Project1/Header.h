1c07c9682e6e89d8d587c2c9106ea021887c3d81
leonovichka <92265733+leonovichka@users.noreply.github.com>
2021-12-28 02:10:36 +0300

    Add files via upload

---
+++ Project1/Header.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "AbstractDeque.h"
+using namespace std;
+
+class NoElementsException :public exception
+{
+public:
+	NoElementsException(const char* msg) :
+		exception(msg) {}
+};
+
+template <typename T>
+struct Node
+{
+	T data;
+	Node* prev, * next;
+};
+
+template <typename T>
+class Deque : AbstractDeque<T>
+{
+public:
+	Deque();
+	Deque(T count);
+	Deque& operator=(Deque&& other);
+	int GetSize();
+	void pushFront(const T a);
+	void pushBack(const T a);
+	void popFront(T& a);
+	void popBack(T& a);
+	void peekFront(T& a);
+	void peekBack(T& a);
+private:
+	Node<T>* head;
+	Node<T>* tail;
+	int count;
+};
+
